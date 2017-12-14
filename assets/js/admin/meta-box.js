f7392fefc4450fc604da59320db66260de04926e
José Augusto Salim <jose@Joses-MacBook-Air.local>
2017-12-14 09:43:50 -0200

    included assets

--- assets/js/admin/meta-box.js
+++ assets/js/admin/meta-box.js
@@ -10,16 +10,16 @@ jQuery(document).ready(function( $ ){
             jQuery('._evoucherwp_singlecode_field').addClass('hide');   
             jQuery('._evoucherwp_codelength_field').removeClass('hide');
         }
-    });
-
+    });    
 
     $('#_evoucherwp_use_default').change( function(){
-        $('#voucher_options input, #voucher_options select').prop('disabled', this.checked);
+        $('#voucher_options input:not(:hidden), #voucher_options select').prop('disabled', this.checked);
         $(this).prop('disabled', false );
+        $('#voucher_options #_evoucherwp_live').prop('disabled', false );
     }).change();
 
     $('#_evoucherwp_use_default_header').change( function(){
-        $('#voucher_header input, #voucher_header textarea').prop('disabled', this.checked);
+        $('#voucher_header input:not(:hidden), #voucher_header textarea').prop('disabled', this.checked);
         $(this).prop('disabled', false );
     }).change();
 });
